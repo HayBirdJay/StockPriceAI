6f4f03d4769ec823c464fdc3851d5004d08a74c1
archie <38737795+archizzl@users.noreply.github.com>
2024-08-13 10:44:17 -0400

    write json to csv

---
+++ json_to_csv.py
@@ -0,0 +1,16 @@
+import json
+import csv
+
+ticker = "AAPL"
+
+with open(f'training_data/{ticker}_prices_formatted.json', 'r') as f:
+    prices = json.loads(f.read())
+    f.close()
+
+with open(f'training_data/{ticker}_prices_csv.json', 'w') as f:
+    csv_writer = csv.writer(f)
+
+    for day in prices:
+        csv_writer.writerow([day, prices[day]])
+    
+    f.close()
